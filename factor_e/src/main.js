7afe83fe350ebb16dba546697db892f0a5e76e85
Mehdi <mehdi.chaouche@le-campus-numerique.fr>
2021-02-03 11:35:33 +0100

    FAC-8 => Installation d'Axios, token d'authentification dans main.js, Login.vue, router.js (TODO)

--- factor_e/src/main.js
+++ factor_e/src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Axios from 'axios'
+// TODO imports
 
 Vue.config.productionTip = false
+Vue.prototype.$http = Axios;
+
+const token = localStorage.getItem('token')
+if (token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+}
 
 new Vue({
   render: h => h(App),
