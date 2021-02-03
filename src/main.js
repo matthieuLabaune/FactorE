0357cabae66c18b5f075b9d31add85d4c131d20b
Matthieu <matthieu.labaune@le-campus-numerique.fr>
2021-02-03 13:13:34 +0100

    Remix -> vue-auth is working

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 import Axios from 'axios'
-// TODO imports
 
-Vue.config.productionTip = false
 Vue.prototype.$http = Axios;
 
-const token = localStorage.getItem('token')
+const token = localStorage.getItem('user-token')
 if (token) {
   Vue.prototype.$http.defaults.headers.common['Authorization'] = token
 }
 
+Vue.config.productionTip = false
+
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
