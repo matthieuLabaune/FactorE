07fe37d6315d7f3be9aff5011cfbd9d610cc19cb
LaurentMarc <zugugbug@gmail.com>
2021-02-04 11:41:39 +0100

    rebase projet en vue 2

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,28 @@
 <template>
   <div id="app">
-    <div class="container">
-      <div id="bar">
-        <h1>Vue Email Editor (Demo)</h1>
-
-        <button v-on:click="saveDesign">Save Design</button>
-        <button v-on:click="exportHtml">Export HTML</button>
-      </div>
-
-      <EmailEditor ref="emailEditor" v-on:load="editorLoaded" />
-    </div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import { EmailEditor } from "vue-email-editor";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "app",
+  name: 'App',
   components: {
-    EmailEditor,
-  },
-  methods: {
-    editorLoaded() {
-      this.$refs.emailEditor.editor.loadDesign({});
-    },
-    saveDesign() {
-      this.$refs.emailEditor.editor.saveDesign((design) => {
-        console.log("saveDesign", design);
-      });
-    },
-    exportHtml() {
-      this.$refs.emailEditor.editor.exportHtml((data) => {
-        console.log("exportHtml", data);
-      });
-    },
-  },
-};
+    HelloWorld
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
